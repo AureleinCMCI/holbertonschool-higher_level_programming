1c21f0c1cd598be267861a75559e06c13b00bcac
Your <you@example.com>
2025-02-13 15:28:19 +0100

    r

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""
-Basic serializati module to save and load Python dictionaries as JSON.
-"""
-
+"""Basic serialization module to save and load Python dictionaries as JSON."""
 
 import json
 import pickle
 
+
 class CustomObject:
-    def __init__(self, name, age, is_student = True):
+    def __init__(self, name, age, is_student=True):
         self.name = name
         self.age = age
         self.is_student = is_student
-    
+
     def display(self):
-        print(f"Name: {self.name}, Age: {self.age}, Student: {self.is_student}")
-    
-    def serialize(self,filename):
+        """Display object attributes."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
         """Sérialise l'instance courante et la sauvegarde dans un fichier."""
         with open(filename, "wb") as file:
             pickle.dump(self, file)
-            
-    @classmethod 
+
+    @classmethod
     def deserialize(cls, filename):
         """Charge une instance sérialisée depuis un fichier et la renvoie."""
         with open(filename, "rb") as file:
