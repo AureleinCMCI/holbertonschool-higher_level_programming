defead1df32a8072255c0970078b5415dc45e56c
Your <you@example.com>
2025-03-06 13:47:13 +0100

    proe

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+
+-- creates the MySQL server user user_0d_1.
+   -- user_0d_1 should have all privileges on your MySQL server
+   -- password set to user_0d_1_pwd
+   -- If the user user_0d_1 already exists, script should not fail
+   
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id) ON DELETE CASCADE
+);
