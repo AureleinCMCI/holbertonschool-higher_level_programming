1409c5e0f1a1d383a99fcd39a8e48b4720e88b0e
Your <you@example.com>
2025-03-06 13:48:00 +0100

    proe

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,7 +3,8 @@
    -- user_0d_1 should have all privileges on your MySQL server
    -- password set to user_0d_1_pwd
    -- If the user user_0d_1 already exists, script should not fail
-   
+
+  
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
