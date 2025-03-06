fc261f395f692dd50055744596127e38f6899242
Your <you@example.com>
2025-03-06 14:31:49 +0100

    proe

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- creates the MySQL server user user_0d_1.
+   -- user_0d_1 should have all privileges on your MySQL server
+   -- password set to user_0d_1_pwd
+   -- If the user user_0d_1 already exists, script should not fail
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
